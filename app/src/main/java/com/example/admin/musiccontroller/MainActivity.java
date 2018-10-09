ad1dbe74e141570ec97bfdd0bfbf03376193c211
Aniket Jadhav <aniket.jadhav.8151@gmail.com>
2018-10-09 14:32:09 +0530

    Comment Removed
    Tested Comment Removed.

--- app/src/main/java/com/example/admin/musiccontroller/MainActivity.java
+++ app/src/main/java/com/example/admin/musiccontroller/MainActivity.java
@@ -13,8 +13,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
-    //Its AJSTyle
+    
     MediaPlayer mp;
     AudioManager audioManager;
     public void PlayClicked(View view){
